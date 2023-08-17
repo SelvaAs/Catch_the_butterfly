f72e8b29d5e03ce3f8826dab76666afc19d49ddd
SelvaAs <136520563+SelvaAs@users.noreply.github.com>
2023-08-16 23:32:47 -0400

    Update MainPage.java

--- app/src/main/java/com/example/catch_the_butterfly/MainPage.java
+++ app/src/main/java/com/example/catch_the_butterfly/MainPage.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
@@ -16,7 +15,6 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainPage extends AppCompatActivity {
