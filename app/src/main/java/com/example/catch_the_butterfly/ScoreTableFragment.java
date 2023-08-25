a158a8ff0addedcfb20caebaca4b87d458a9f22c
SelvaAs <136520563+SelvaAs@users.noreply.github.com>
2023-08-24 23:27:54 -0400

    Update ScoreTableFragment.java

--- app/src/main/java/com/example/catch_the_butterfly/ScoreTableFragment.java
+++ app/src/main/java/com/example/catch_the_butterfly/ScoreTableFragment.java
@@ -6,13 +6,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 
 
